b7fde43f6f498d6d81bc6b8906bd726ab3e59b7d
Bo Bayles <bbayles@gmail.com>
2023-09-08 11:48:08 -0500

    Add update-wpt.sh

---
+++ update-wpt.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -e
+
+BASE_DIR=$(pwd)
+WPT_DIR="$BASE_DIR/tests/files"
+
+WORKSPACE=$(mktemp -d 2> /dev/null || mktemp -d -t 'tmp')
+
+cleanup () {
+  EXIT_CODE=$?
+  [ -d "$WORKSPACE" ] && rm -rf "$WORKSPACE"
+  exit $EXIT_CODE
+}
+
+trap cleanup INT TERM EXIT
+
+cd "$WORKSPACE"
+git clone \
+  --no-checkout \
+  --depth=1 \
+  --filter=blob:none \
+  --sparse \
+  https://github.com/web-platform-tests/wpt.git wpt
+cd wpt
+git sparse-checkout add "url/resources"
+git checkout
+cp url/resources/urltestdata.json "$WPT_DIR"
